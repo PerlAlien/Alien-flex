c839f82578f8e11d717f0280559acea06536cf1f
Graham Ollis <plicease@cpan.org>
2014-11-19 14:20:32 -0500

    initial implementation

---
+++ inc/My/ModuleBuild.pm
@@ -0,0 +1,120 @@
+package My::ModuleBuild;
+
+use strict;
+use warnings;
+use base qw( Alien::Base::ModuleBuild );
+use File::chdir;
+use Capture::Tiny qw( capture_stderr );
+use File::Spec;
+
+our $quiet = 0;
+
+my $patch = $^O eq 'MSWin32' ? 'patch --binary' : 'patch';
+
+sub new
+{
+  my($class, %args) = @_;
+  
+  $args{alien_name} = 'flex';
+  $args{alien_build_commands} = [
+    '%c --prefix=%s',
+    'make',
+  ];
+  $args{alien_install_commands} = [
+    'make install',
+  ];
+  $args{alien_repository} = {
+    protocol       => 'http',
+    host           => 'sourceforge.net',
+    location       => '/projects/flex/files/',
+    exact_filename => 'flex-2.5.39.tar.gz',
+  };
+
+  my $self = $class->SUPER::new(%args);
+  
+  $self;
+}
+
+sub _short ($)
+{
+  $_[0] =~ /\s+/ ? Win32::GetShortPathName( $_[0] ) : $_[0];
+}
+
+sub alien_check_installed_version
+{
+  my($self) = @_;
+
+  my @paths = ([]);
+
+  if($^O eq 'MSWin32')
+  {
+    eval '# line '. __LINE__ . ' "' . __FILE__ . "\n" . q{
+      use strict;
+      use warnings;
+      use Win32API::Registry 0.21 qw( :ALL );
+      
+      my @uninstall_key_names = qw(
+        software\wow6432node\microsoft\windows\currentversion\uninstall
+        software\microsoft\windows\currentversion\uninstall
+      );
+      
+      foreach my $uninstall_key_name (@uninstall_key_names)
+      {
+        my $uninstall_key;
+        RegOpenKeyEx( HKEY_LOCAL_MACHINE, $uninstall_key_name, 0, KEY_READ, $uninstall_key ) || next;
+        
+        my $pos = 0;
+        my($subkey, $class, $time) = ('','','');
+        my($namSiz, $clsSiz) = (1024,1024);
+        while(RegEnumKeyEx( $uninstall_key, $pos++, $subkey, $namSiz, [], $class, $clsSiz, $time))
+        {
+          next unless $subkey =~ /^flex/i;
+          my $flex_key;
+          RegOpenKeyEx( $uninstall_key, $subkey, 0, KEY_READ, $flex_key ) || next;
+          
+          my $data;
+          if(RegQueryValueEx($flex_key, "InstallLocation", [], REG_SZ, $data, [] ))
+          {
+            push @paths, [File::Spec->catdir(_short $data, "bin")];
+          }
+          
+          RegCloseKey( $flex_key );
+        }
+        RegCloseKey($uninstall_key);
+      }
+    };
+    warn $@ if $@;
+    
+    push @paths, map { [_short $ENV{$_}, qw( GnuWin32 bin )] } grep { defined $ENV{$_} } qw[ ProgramFiles ProgramFiles(x86) ];
+    push @paths, ['C:\\GnuWin32\\bin'];
+    
+  }
+
+  unless($quiet)
+  {
+    print "try system paths:\n";
+    print "  - ", $_, "\n" for map { $_ eq '' ? 'PATH' : $_ } map { File::Spec->catdir(@$_) } @paths;
+  }
+  
+  foreach my $path (@paths)
+  {
+    my @stdout;
+    my $exe = File::Spec->catfile(@$path, 'flex');
+    my $stderr = capture_stderr {
+      @stdout = `$exe --version`;
+    };
+    if(defined $stdout[0] && $stdout[0] =~ /^flex/ && $stdout[0] =~ /([0-9\.]+)$/)
+    {
+      $self->config_data( flex_system_path => File::Spec->catdir(@$path) ) if @$path;
+      return $1;
+    }
+  }
+  return;
+}
+
+sub alien_check_built_version
+{
+  $CWD[-1] =~ /^flex-(.*)$/ ? $1 : 'unknown';
+}
+
+1;
--- lib/Alien/flex.pm
+++ lib/Alien/flex.pm
@@ -2,8 +2,45 @@ package Alien::flex;
 
 use strict;
 use warnings;
+use base qw( Alien::Base );
 
-# ABSTRACT: Find and install flex
+# ABSTRACT: Find or build flex
 # VERSION
+
+=head1 SYNOPSIS
+
+From a Perl script
+
+ use Alien::flex;
+ use Env qw( @PATH );
+ unshift @PATH, Alien::flex->bin_dir;  # flex is now in your path
+
+From Alien::Base Build.PL
+
+ use Alien:Base::ModuleBuild;
+ my $builder = Module::Build->new(
+   ...
+   alien_bin_requires => [ 'Alien::flex' ],
+   ...
+ );
+ $builder->create_build_script;
+
+=head1 DESCRIPTION
+
+This package can be used by other CPAN modules that require flex.
+
+=cut
+
+sub bin_dir
+{
+  my($class) = @_;
+  if($class->install_type('system'))
+  {
+    my $path = $class->config('bison_system_path');
+    return ($path) if $path;
+  }
+
+  return $class->SUPER::bin_dir;
+}
 
 1;
